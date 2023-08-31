13e18a9e4f406aaa9beb1b0707b436f172f4675d
Maksim Kropanov <kropanov2012@gmail.com>
2023-08-31 10:16:56 +0300

    App bar was added

--- web/src/hoc/Layout/Layout.tsx
+++ web/src/hoc/Layout/Layout.tsx
@@ -1,10 +1,12 @@
 import React, {ReactElement} from 'react';
+import MenuAppBar from "../../components/AppBar/AppBar";
 
 type LayoutProps = { children: ReactElement; };
 
 export default function Layout(props: LayoutProps) {
     return (
         <>
+            <MenuAppBar/>
             {props.children}
         </>
     );
