e6a0142fd06658a44cf8050d8f6d9489e4e09f9a
Maksim Kropanov <kropanov2012@gmail.com>
2023-08-31 12:46:02 +0300

    Code refactoring

--- web/src/components/AppBar/AppBar.tsx
+++ web/src/components/AppBar/AppBar.tsx
@@ -1,8 +1,6 @@
 import * as React from 'react';
-import { styled, useTheme, Theme, CSSObject } from '@mui/material/styles';
+import {useTheme} from '@mui/material/styles';
 import Box from '@mui/material/Box';
-import MuiDrawer from '@mui/material/Drawer';
-import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import List from '@mui/material/List';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -21,77 +19,7 @@ import PrecisionManufacturingIcon from '@mui/icons-material/PrecisionManufacturi
 import NextPlanIcon from '@mui/icons-material/NextPlan';
 import NavigationIcon from '@mui/icons-material/Navigation';
 import {ReactElement} from "react";
-
-const drawerWidth = 240;
-
-const openedMixin = (theme: Theme): CSSObject => ({
-    width: drawerWidth,
-    transition: theme.transitions.create('width', {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.enteringScreen,
-    }),
-    overflowX: 'hidden',
-});
-
-const closedMixin = (theme: Theme): CSSObject => ({
-    transition: theme.transitions.create('width', {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-    }),
-    overflowX: 'hidden',
-    width: `calc(${theme.spacing(7)} + 1px)`,
-    [theme.breakpoints.up('sm')]: {
-        width: `calc(${theme.spacing(8)} + 1px)`,
-    },
-});
-
-const DrawerHeader = styled('div')(({ theme }) => ({
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    padding: theme.spacing(0, 1),
-    // necessary for content to be below app bar
-    ...theme.mixins.toolbar,
-}));
-
-interface AppBarProps extends MuiAppBarProps {
-    open?: boolean;
-}
-
-const AppBar = styled(MuiAppBar, {
-    shouldForwardProp: (prop) => prop !== 'open',
-})<AppBarProps>(({ theme, open }) => ({
-    zIndex: theme.zIndex.drawer + 1,
-    transition: theme.transitions.create(['width', 'margin'], {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-    }),
-    ...(open && {
-        marginLeft: drawerWidth,
-        width: `calc(100% - ${drawerWidth}px)`,
-        transition: theme.transitions.create(['width', 'margin'], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-    }),
-}));
-
-const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
-    ({ theme, open }) => ({
-        width: drawerWidth,
-        flexShrink: 0,
-        whiteSpace: 'nowrap',
-        boxSizing: 'border-box',
-        ...(open && {
-            ...openedMixin(theme),
-            '& .MuiDrawer-paper': openedMixin(theme),
-        }),
-        ...(!open && {
-            ...closedMixin(theme),
-            '& .MuiDrawer-paper': closedMixin(theme),
-        }),
-    }),
-);
+import {AppBar, Drawer, DrawerHeader} from "./StyledComponents/StyledComponents";
 
 const data = [
     {
@@ -112,11 +40,12 @@ const data = [
     },
 ];
 
-type LayoutProps = { children: ReactElement; };
+type MenuAppBarProps = { children: ReactElement; };
 
-export default function MenuAppBar(props: LayoutProps) {
+export default function MenuAppBar(props: MenuAppBarProps) {
     const theme = useTheme();
     const [open, setOpen] = React.useState(false);
+    const [value, setValue] = React.useState("Navigation");
 
     const handleDrawerOpen = () => {
         setOpen(true);
@@ -124,6 +53,11 @@ export default function MenuAppBar(props: LayoutProps) {
 
     const handleDrawerClose = () => {
         setOpen(false);
+    };
+
+    const handleButtonClick = (event: React.MouseEvent<HTMLElement>) => {
+        setValue(event.currentTarget.getAttribute("data-text") || "");
+        handleDrawerClose();
     };
 
     return (
@@ -144,7 +78,7 @@ export default function MenuAppBar(props: LayoutProps) {
                         <MenuIcon />
                     </IconButton>
                     <Typography variant="h6" noWrap component="div">
-                        Mini variant drawer
+                        {value}
                     </Typography>
                 </Toolbar>
             </AppBar>
@@ -157,7 +91,13 @@ export default function MenuAppBar(props: LayoutProps) {
                 <Divider />
                 <List>
                     {data.map((item) => (
-                        <ListItem key={item.text} disablePadding sx={{ display: 'block' }}>
+                        <ListItem
+                            key={item.text}
+                            disablePadding
+                            sx={{ display: 'block' }}
+                            data-text={item.text}
+                            onClick={(event) => handleButtonClick(event)}
+                        >
                             <ListItemButton
                                 sx={{
                                     minHeight: 48,
