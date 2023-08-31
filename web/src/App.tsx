90d2e809311c0c7edf6ecda824f300fc972364fe
Maksim Kropanov <94756270+Kropanov@users.noreply.github.com>
2023-08-31 08:49:16 +0300

    Add react router dom v6 (#4)

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import Main from "./pages/main/Main";
 import Layout from "./hoc/Layout/Layout";
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+import ErrorPage from "./pages/error-page/ErrorPage";
+
+const router = createBrowserRouter([
+    {
+        path: "*",
+        element: <Main />,
+        errorElement: <ErrorPage />
+    },
+]);
 
 function App() {
     return (
         <Layout>
-            <Main />
+            <RouterProvider router={router} />
         </Layout>
     );
 }
