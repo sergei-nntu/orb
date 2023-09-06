914d79d9a04c7dc94c2ae604372cd3d7cd2542f9
Maksim Kropanov <94756270+Kropanov@users.noreply.github.com>
2023-09-06 10:44:11 +0300

    Feature/blockly dark theme (#14)
    * Fix a problem after merging
    
    * Add dark theme
    
    * Change theme based on user preference
    
    * Small fix

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -55,9 +55,7 @@ function App() {
     return (
         <ThemeProvider theme={theme}>
             <CssBaseline />
-            <MenuAppBar>
-                <RouterProvider router={router} />
-            </MenuAppBar>
+            <RouterProvider router={router} />
         </ThemeProvider>
     );
 }
