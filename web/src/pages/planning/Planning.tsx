7db57d842740b9b3492809c7c05f7a734d2059c1
Maksim Kropanov <94756270+Kropanov@users.noreply.github.com>
2023-09-07 13:41:52 +0300

    Feature/create custom components (#16)
    * Fix: warning (failed to parse source map) was removed
    
    * Add common and custom components
    
    * Create all custom blocks
    
    * Code refactoring

--- web/src/pages/planning/Planning.tsx
+++ web/src/pages/planning/Planning.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
 import BlocklyEditor from './BlocklyEditor/BlocklyEditor';
+import "./CustomBlocks/DelayBlock/DelayBlock";
+import "./CustomBlocks/PositionBlock/PositionBlock";
+import "./CustomBlocks/GripperStepBlock/GripperStepBlock";
+
+const toolboxXML =
+    "<xml>" +
+        "<block type='controls_if'></block>" +
+        "<block type='logic_boolean'></block>" +
+        "<block type='math_number' gap='30'>" +
+            "<field name='NUM'>1</field>" +
+        "</block>" +
+        "<block type='controls_whileUntil'></block>" +
+        "<block type='controls_for'></block>" +
+        "<block type='set_position'></block>" +
+        "<block type='delay'></block>" +
+        "<block type='set_gripper_step'></block>" +
+    "</xml>";
 
 export default function Planning() {
-    const toolboxXML = "<xml><block type='controls_if'></block></xml>";
     return (
         <BlocklyEditor toolboxXML={toolboxXML} />
     );
