ed5da2619dc3d2bd8fe1c63808c6d3c2f687e8b5
Maksim Kropanov <kropanov2012@gmail.com>
2023-09-07 21:34:28 +0300

    Layout was returned as root component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -5,18 +5,18 @@ import {
 } from "react-router-dom";
 import ErrorPage from "./pages/error-page/ErrorPage";
 import Planning from "./pages/planning/Planning";
-import MenuAppBar from "./components/AppBar/AppBar";
 import Navigation from "./pages/navigation/Navigation";
 import Manipulator from "./pages/manipulator/Manipulator";
 import QRPage from "./pages/qr-page/QRPage";
 import useMediaQuery from '@mui/material/useMediaQuery';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import Layout from "./hoc/Layout/Layout";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <MenuAppBar />,
+        element: <Layout />,
         errorElement: <ErrorPage />,
         children: [
             {
