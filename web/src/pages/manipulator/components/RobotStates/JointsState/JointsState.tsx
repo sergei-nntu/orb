9b86f4ab3f728c76fe766ded2c418b42cc5a2a43
Maksim Kropanov <kropanov2012@gmail.com>
2023-09-14 11:10:39 +0300

    Fix: edit min/max/step of joints

--- web/src/pages/manipulator/components/RobotStates/JointsState/JointsState.tsx
+++ web/src/pages/manipulator/components/RobotStates/JointsState/JointsState.tsx
@@ -68,12 +68,12 @@ export default function JointsState() {
     const handleBlur: HandleBlurFunction = (value, setValue) => {
         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-ignore
-        if (value < 0) {
-            setValue(0);
+        if (value < -130) {
+            setValue(-130);
             // eslint-disable-next-line @typescript-eslint/ban-ts-comment
             // @ts-ignore
-        } else if (value > 100) {
-            setValue(100);
+        } else if (value > 130) {
+            setValue(130);
         }
     };
 
@@ -90,18 +90,21 @@ export default function JointsState() {
                             value={joint0Value}
                             onChange={handleJoint0Change}
                             aria-labelledby="input-slider"
+                            min={-130}
+                            max={130}
                         />
                     </Grid>
                     <Grid item>
                         <Input
+                            sx={{minWidth: "50px"}}
                             value={joint0Value}
                             size="small"
                             onChange={(e) => handleInputChange(e, setJoint0Value)}
                             onBlur={() => handleBlur(joint0Value, setJoint0Value)}
                             inputProps={{
-                                step: 10,
-                                min: 0,
-                                max: 100,
+                                step: 1,
+                                min: -130,
+                                max: 130,
                                 type: 'number',
                                 'aria-labelledby': 'input-slider',
                             }}
@@ -117,18 +120,21 @@ export default function JointsState() {
                             value={joint1Value}
                             onChange={handleJoint1Change}
                             aria-labelledby="input-slider"
+                            min={-90}
+                            max={90}
                         />
                     </Grid>
                     <Grid item>
                         <Input
+                            sx={{minWidth: "50px"}}
                             value={joint1Value}
                             size="small"
                             onChange={(e) => handleInputChange(e, setJoint1Value)}
                             onBlur={() => handleBlur(joint1Value, setJoint1Value)}
                             inputProps={{
-                                step: 10,
-                                min: 0,
-                                max: 100,
+                                step: 1,
+                                min: -90,
+                                max: 90,
                                 type: 'number',
                                 'aria-labelledby': 'input-slider',
                             }}
@@ -144,18 +150,21 @@ export default function JointsState() {
                             value={joint2Value}
                             onChange={handleJoint2Change}
                             aria-labelledby="input-slider"
+                            min={-130}
+                            max={130}
                         />
                     </Grid>
                     <Grid item>
                         <Input
+                            sx={{minWidth: "50px"}}
                             value={joint2Value}
                             size="small"
                             onChange={(e) => handleInputChange(e, setJoint2Value)}
                             onBlur={() => handleBlur(joint2Value, setJoint2Value)}
                             inputProps={{
-                                step: 10,
-                                min: 0,
-                                max: 100,
+                                step: 1,
+                                min: -130,
+                                max: 130,
                                 type: 'number',
                                 'aria-labelledby': 'input-slider',
                             }}
@@ -171,18 +180,21 @@ export default function JointsState() {
                             value={joint3Value}
                             onChange={handleJoint3Change}
                             aria-labelledby="input-slider"
+                            min={-130}
+                            max={130}
                         />
                     </Grid>
                     <Grid item>
                         <Input
+                            sx={{minWidth: "50px"}}
                             value={joint3Value}
                             size="small"
                             onChange={(e) => handleInputChange(e, setJoint3Value)}
                             onBlur={() => handleBlur(joint3Value, setJoint3Value)}
                             inputProps={{
-                                step: 10,
-                                min: 0,
-                                max: 100,
+                                step: 1,
+                                min: -130,
+                                max: 130,
                                 type: 'number',
                                 'aria-labelledby': 'input-slider',
                             }}
@@ -198,18 +210,21 @@ export default function JointsState() {
                             value={joint4Value}
                             onChange={handleJoint4Change}
                             aria-labelledby="input-slider"
+                            min={-130}
+                            max={130}
                         />
                     </Grid>
                     <Grid item>
                         <Input
+                            sx={{minWidth: "50px"}}
                             value={joint4Value}
                             size="small"
                             onChange={(e) => handleInputChange(e, setJoint4Value)}
                             onBlur={() => handleBlur(joint4Value, setJoint4Value)}
                             inputProps={{
-                                step: 10,
-                                min: 0,
-                                max: 100,
+                                step: 1,
+                                min: -130,
+                                max: 130,
                                 type: 'number',
                                 'aria-labelledby': 'input-slider',
                             }}
@@ -225,18 +240,21 @@ export default function JointsState() {
                             value={joint5Value}
                             onChange={handleJoint5Change}
                             aria-labelledby="input-slider"
+                            min={-130}
+                            max={130}
                         />
                     </Grid>
                     <Grid item>
                         <Input
+                            sx={{minWidth: "50px"}}
                             value={joint5Value}
                             size="small"
                             onChange={(e) => handleInputChange(e, setJoint5Value)}
                             onBlur={() => handleBlur(joint5Value, setJoint5Value)}
                             inputProps={{
-                                step: 10,
-                                min: 0,
-                                max: 100,
+                                step: 1,
+                                min: -130,
+                                max: 130,
                                 type: 'number',
                                 'aria-labelledby': 'input-slider',
                             }}
