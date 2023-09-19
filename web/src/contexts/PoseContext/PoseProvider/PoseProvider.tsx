69fa0cc15162d500aeeaf420a168a18e05aab833
Maksim Kropanov <kropanov2012@gmail.com>
2023-09-19 08:39:37 +0300

    Created useEffect for sending data to server in Pose Provider

--- web/src/contexts/PoseContext/PoseProvider/PoseProvider.tsx
+++ web/src/contexts/PoseContext/PoseProvider/PoseProvider.tsx
@@ -1,6 +1,8 @@
-import React, {useMemo, useReducer} from 'react';
+import React, {useEffect, useMemo, useReducer} from 'react';
 import reducer from "../PoseReducer/PoseReducer";
 import {PoseContext} from "../PoseContext";
+import useHttp from "../../../hooks/Http/Http";
+import {IPose} from "../../../types/appTypes";
 
 type PoseProviderProps = {
     children: React.ReactNode
@@ -24,7 +26,33 @@ function PoseProvider(props: PoseProviderProps) {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
     const [state, dispatch] = useReducer(reducer, initialState);
+    const {request} = useHttp();
     const value = useMemo(() => ({state, dispatch}), [state]);
+
+    const sendStateToServer = async (state: IPose) => {
+        try {
+            const options = {
+            method: "POST",
+            body: JSON.stringify({
+                    "x": state.position.x,
+                    "y": state.position.y,
+                    "z": state.position.z,
+                    "pitch": state.orientation.pitch,
+                    "roll": state.orientation.roll,
+                    "yaw": state.orientation.yaw
+                })
+            };
+
+            const result = await request("/convert_pose", options);
+            console.log(result);
+        } catch (error) {
+            console.error("Error: ", error);
+        }
+    };
+
+    useEffect(() => {
+        sendStateToServer(state).then(r => console.log(r));
+    }, [state]);
 
     return (
         <PoseContext.Provider value={value}>
