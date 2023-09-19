d544ffc270df23ddcf95cb206da587115dd88a0f
Maksim Kropanov <94756270+Kropanov@users.noreply.github.com>
2023-09-19 19:42:19 +0300

    Feature/create pose state (#26)
    * Create PoseContext.ts, PoseProvider.tsx, PoseReducer.ts
    
    * Use provider with context and reducer
    
    * Position actions and types have been created
    
    * Complete pose actions, provide it in position and orientation components
    
    * Complete getting gripper state
    
    * Created useEffect for sending data to server in Pose Provider
    
    * Convert degrees of gripper state to radians. Send gripper state to server either

---
+++ web/src/contexts/PoseContext/PoseProvider/PoseProvider.tsx
@@ -0,0 +1,65 @@
+import React, {useEffect, useMemo, useReducer} from 'react';
+import reducer from "../PoseReducer/PoseReducer";
+import {PoseContext} from "../PoseContext";
+import useHttp from "../../../hooks/Http/Http";
+import {IPose} from "../../../types/appTypes";
+
+type PoseProviderProps = {
+    children: React.ReactNode
+};
+
+function PoseProvider(props: PoseProviderProps) {
+    const initialState = {
+        position: {
+            x: 0.0,
+            y: 0.0,
+            z: 0.0
+        },
+        orientation: {
+            pitch: 0.0,
+            roll: 0.0,
+            yaw: 0.0
+        },
+        gripper_state: 0.0
+    };
+
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    const [state, dispatch] = useReducer(reducer, initialState);
+    const {request} = useHttp();
+    const value = useMemo(() => ({state, dispatch}), [state]);
+
+    const sendStateToServer = async (state: IPose) => {
+        try {
+            const options = {
+                method: "POST",
+                body: JSON.stringify({
+                    "x": state.position.x,
+                    "y": state.position.y,
+                    "z": state.position.z,
+                    "pitch": state.orientation.pitch,
+                    "roll": state.orientation.roll,
+                    "yaw": state.orientation.yaw,
+                    "gripper": state.gripper_state
+                })
+            };
+
+            const result = await request("/convert_pose", options);
+            console.log("Result: ", result);
+        } catch (error) {
+            console.error("Error: ", error);
+        }
+    };
+
+    useEffect(() => {
+        sendStateToServer(state).then(r => console.log(r));
+    }, [state]);
+
+    return (
+        <PoseContext.Provider value={value}>
+            {props.children}
+        </PoseContext.Provider>
+    );
+}
+
+export default PoseProvider;
---
+++ web/src/types/appTypes.ts
@@ -0,0 +1,46 @@
+type Action<K, V = void> = V extends void ? { type: K } : { type: K } & V;
+
+export interface IPose {
+    position: {
+        x: number,
+        y: number,
+        z: number
+    },
+    orientation: {
+        pitch: number,
+        roll: number,
+        yaw: number
+    },
+    gripper_state: number
+}
+
+export enum PoseChange {
+    POSITION_X_UP = 'POSITION_X_UP',
+    POSITION_X_DOWN = 'POSITION_X_DOWN',
+    POSITION_Y_UP = 'POSITION_Y_UP',
+    POSITION_Y_DOWN = 'POSITION_Y_DOWN',
+    POSITION_Z_UP = 'POSITION_Z_UP',
+    POSITION_Z_DOWN = 'POSITION_Z_DOWN',
+    ORIENTATION_PITCH_UP = 'ORIENTATION_PITCH_UP',
+    ORIENTATION_PITCH_DOWN = 'ORIENTATION_PITCH_DOWN',
+    ORIENTATION_ROLL_UP = 'ORIENTATION_ROLL_UP',
+    ORIENTATION_ROLL_DOWN = 'ORIENTATION_ROLL_DOWN',
+    ORIENTATION_YAW_UP = 'ORIENTATION_YAW_UP',
+    ORIENTATION_YAW_DOWN = 'ORIENTATION_YAW_DOWN',
+    SET_GRIPPER_STATE = 'SET_GRIPPER_STATE',
+}
+
+export type PoseActionType =
+    | Action<PoseChange.POSITION_X_UP>
+    | Action<PoseChange.POSITION_X_DOWN>
+    | Action<PoseChange.POSITION_Y_UP>
+    | Action<PoseChange.POSITION_Y_DOWN>
+    | Action<PoseChange.POSITION_Z_UP>
+    | Action<PoseChange.POSITION_Z_DOWN>
+    | Action<PoseChange.ORIENTATION_PITCH_UP>
+    | Action<PoseChange.ORIENTATION_PITCH_DOWN>
+    | Action<PoseChange.ORIENTATION_ROLL_UP>
+    | Action<PoseChange.ORIENTATION_ROLL_DOWN>
+    | Action<PoseChange.ORIENTATION_YAW_UP>
+    | Action<PoseChange.ORIENTATION_YAW_DOWN>
+    | Action<PoseChange.SET_GRIPPER_STATE, { value: number }>;
