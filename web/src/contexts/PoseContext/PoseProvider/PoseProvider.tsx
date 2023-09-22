8d268670587be755f09ee4ff832be9825f95b970
Maksim Kropanov <kropanov2012@gmail.com>
2023-09-22 09:56:40 +0300

    PrevState was moved to Pose component

--- web/src/contexts/PoseContext/PoseProvider/PoseProvider.tsx
+++ web/src/contexts/PoseContext/PoseProvider/PoseProvider.tsx
@@ -1,10 +1,6 @@
-import React, {useContext, useEffect, useMemo, useReducer, useState} from 'react';
+import React, {useMemo, useReducer} from 'react';
 import reducer from "../PoseReducer/PoseReducer";
 import {PoseContext} from "../PoseContext";
-import useHttp from "../../../hooks/Http/Http";
-import {IPose} from "../../../types/appTypes";
-import { NotificationContext } from '../../NotificationContext/NotificationContext';
-import { NOTIFICATION } from '../../../constants';
 
 type PoseProviderProps = {
     children: React.ReactNode
@@ -25,49 +21,10 @@ function PoseProvider(props: PoseProviderProps) {
         gripper_state: 0.0
     };
 
-    const {dispatchNotification} = useContext(NotificationContext);
-
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
     const [state, dispatch] = useReducer(reducer, initialState);
-    const [prevState, setPrevState] = useState<IPose>(initialState);
-    const {request} = useHttp();
     const value = useMemo(() => ({state, dispatch}), [state]);
-
-    const sendStateToServer = async (state: IPose) => {
-        try {
-            const options = {
-                method: "POST",
-                body: JSON.stringify({
-                    "x": state.position.x,
-                    "y": state.position.y,
-                    "z": state.position.z,
-                    "pitch": state.orientation.pitch,
-                    "roll": state.orientation.roll,
-                    "yaw": state.orientation.yaw,
-                    "gripper": state.gripper_state
-                })
-            };
-
-            const {execute} = await request("/convert_pose", options);
-            
-            if (execute) {
-                setPrevState(state);
-            }
-            else {
-                dispatchNotification({type: NOTIFICATION.NO_MOVE_TO_POSITION});
-                // There should be dispatch
-                console.log(prevState);
-            }
-
-        } catch (error) {
-            console.error("Error: ", error);
-        }
-    };
-
-    useEffect(() => {
-        sendStateToServer(state).then(r => console.log(r));
-    }, [state]);
 
     return (
         <PoseContext.Provider value={value}>
