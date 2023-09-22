22bcc45fddc945a44d33418bd93854fde0bd0b69
Maksim Kropanov <kropanov2012@gmail.com>
2023-09-22 11:09:45 +0300

    It was returned to the last commit

--- web/src/contexts/PoseContext/PoseProvider/PoseProvider.tsx
+++ web/src/contexts/PoseContext/PoseProvider/PoseProvider.tsx
@@ -1,6 +1,10 @@
-import React, {useMemo, useReducer} from 'react';
+import React, {useContext, useEffect, useMemo, useReducer} from 'react';
 import reducer from "../PoseReducer/PoseReducer";
 import {PoseContext} from "../PoseContext";
+import useHttp from "../../../hooks/Http/Http";
+import {IPose} from "../../../types/appTypes";
+import { NotificationContext } from '../../NotificationContext/NotificationContext';
+import { NOTIFICATION } from '../../../constants';
 
 type PoseProviderProps = {
     children: React.ReactNode
@@ -21,10 +25,44 @@ function PoseProvider(props: PoseProviderProps) {
         gripper_state: 0.0
     };
 
+    const {dispatchNotification} = useContext(NotificationContext);
+
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
     const [state, dispatch] = useReducer(reducer, initialState);
+    const {request} = useHttp();
     const value = useMemo(() => ({state, dispatch}), [state]);
+
+    const sendStateToServer = async (state: IPose) => {
+        try {
+            const options = {
+                method: "POST",
+                body: JSON.stringify({
+                    "x": state.position.x,
+                    "y": state.position.y,
+                    "z": state.position.z,
+                    "pitch": state.orientation.pitch,
+                    "roll": state.orientation.roll,
+                    "yaw": state.orientation.yaw,
+                    "gripper": state.gripper_state
+                })
+            };
+
+            const {execute} = await request("/convert_pose", options);
+            
+            if (!execute) {
+                dispatchNotification({type: NOTIFICATION.NO_MOVE_TO_POSITION});
+
+            }
+
+        } catch (error) {
+            console.error("Error: ", error);
+        }
+    };
+
+    useEffect(() => {
+        sendStateToServer(state).then(r => console.log(r));
+    }, [state]);
 
     return (
         <PoseContext.Provider value={value}>
--- web/src/pages/manipulator/components/Pose/Pose.tsx
+++ web/src/pages/manipulator/components/Pose/Pose.tsx
@@ -1,55 +1,11 @@
-import React, {useContext, useEffect, useRef} from 'react';
+import React from 'react';
 import {Item} from "../StyledComponents/StyledComponents";
 import {Grid} from "@mui/material";
 import Position from "./Position/Position";
 import Orientation from "./Orientation/Orientation";
 import EndEffectorState from "../RobotStates/EndEffectorState/EndEffectorState";
-import {PoseContext} from "../../../../contexts/PoseContext/PoseContext";
-import {IPose, PoseChange} from "../../../../types/appTypes";
-import {NOTIFICATION} from "../../../../constants";
-import useHttp from "../../../../hooks/Http/Http";
-import {NotificationContext} from "../../../../contexts/NotificationContext/NotificationContext";
 
 export default function Pose() {
-    const {state, dispatch} = useContext(PoseContext);
-    const prevStateRef= useRef<IPose>(state);
-    const {dispatchNotification} = useContext(NotificationContext);
-    const {request} = useHttp();
-
-    useEffect(() => {
-        sendPoseToServer(state).then(r => console.log(r));
-    }, [state]);
-
-    const sendPoseToServer = async (state: IPose) => {
-        try {
-            const options = {
-                method: "POST",
-                body: JSON.stringify({
-                    "x": state.position.x,
-                    "y": state.position.y,
-                    "z": state.position.z,
-                    "pitch": state.orientation.pitch,
-                    "roll": state.orientation.roll,
-                    "yaw": state.orientation.yaw,
-                    "gripper": state.gripper_state
-                })
-            };
-
-            const {execute} = await request("/convert_pose", options);
-
-            if (execute) {
-                prevStateRef.current = {...state};
-            }
-            else {
-                dispatchNotification({type: NOTIFICATION.NO_MOVE_TO_POSITION});
-                dispatch({type: PoseChange.SET_PREV_STATE, prevState: prevStateRef.current });
-            }
-
-        } catch (error) {
-            console.error("Error: ", error);
-        }
-    };
-
     return (
         <Grid item xs={3}>
             <Item
