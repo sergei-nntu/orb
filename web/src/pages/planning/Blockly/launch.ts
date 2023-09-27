ad2894a9e8dcef3cf37ca4994ec905ec6d594d6e
Maxim Kropanov <kropanov2012@gmail.com>
2023-09-27 20:11:28 +0300

    Add extra functions

--- web/src/pages/planning/Blockly/launch.ts
+++ web/src/pages/planning/Blockly/launch.ts
@@ -2,8 +2,18 @@ import {KEY} from "../../../constants";
 
 // There is no option to call those functions other way
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-function delay(num: number) {
-    alert(num);
+function delay(value: number) {
+    alert(value);
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function setGripperState(value: number) {
+    alert(`setGripperState:${value}`);
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function setPosition(x: number, y: number, z: number, pitch: number, roll: number, yaw: number) {
+    alert(`setPosition(${x}, ${y}, ${z}, ${pitch}, ${roll}, ${yaw})`);
 }
 
 export const ExecuteBlockly = () => {
