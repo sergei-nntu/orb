1383fef9633b39ba53aa2aaee96a99b0a59ced75
Maxim Kropanov <kropanov2012@gmail.com>
2023-11-16 16:45:29 +0300

    Fix some errors

--- web/src/pages/manipulator/components/RobotStates/EndEffectorState/EndEffectorState.tsx
+++ web/src/pages/manipulator/components/RobotStates/EndEffectorState/EndEffectorState.tsx
@@ -18,32 +18,32 @@ export default function EndEffectorState() {
             <Grid container sx={{ mt: 1, mb: 1 }}>
                 <Grid item xs={6}>
                     <Typography noWrap variant="subtitle2">
-                        <StyledTag>x</StyledTag>: {state.position.x}
+                        <StyledTag>x</StyledTag>: {state?.position.x}
                     </Typography>
                 </Grid>
                 <Grid item xs={6}>
                     <Typography noWrap variant="subtitle2">
-                        <StyledTag>pitch</StyledTag>: {state.orientation.pitch}
+                        <StyledTag>pitch</StyledTag>: {state?.orientation.pitch}
                     </Typography>
                 </Grid>
                 <Grid item xs={6}>
                     <Typography noWrap variant="subtitle2">
-                        <StyledTag>y</StyledTag>: {state.position.y}
+                        <StyledTag>y</StyledTag>: {state?.position.y}
                     </Typography>
                 </Grid>
                 <Grid item xs={6}>
                     <Typography noWrap variant="subtitle2">
-                        <StyledTag>roll</StyledTag>: {state.orientation.roll}
+                        <StyledTag>roll</StyledTag>: {state?.orientation.roll}
                     </Typography>
                 </Grid>
                 <Grid item xs={6}>
                     <Typography noWrap variant="subtitle2">
-                        <StyledTag>z</StyledTag>: {state.position.z}
+                        <StyledTag>z</StyledTag>: {state?.position.z}
                     </Typography>
                 </Grid>
                 <Grid item xs={6}>
                     <Typography noWrap variant="subtitle2">
-                        <StyledTag>yaw</StyledTag>: {state.orientation.yaw}
+                        <StyledTag>yaw</StyledTag>: {state?.orientation.yaw}
                     </Typography>
                 </Grid>
             </Grid>
