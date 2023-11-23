f87dd367e744a283561f3149f84a4aa867e9a81e
2lemetry-work <2lemetry-work@Mac-mini-2lemetry-work.local>
2023-11-23 15:30:13 +0300

    Adding leg models & Working with models movements & Code refactoring

--- web/src/pages/dog-page/DogMain.tsx
+++ web/src/pages/dog-page/DogMain.tsx
@@ -2,14 +2,5 @@ import React from 'react';
 // import { useControls } from 'leva';
 import Dog from './Dog';
 export default function Manipulator() {
-    // const control = useControls('leg 1', {
-    //     leg: {
-    //         value: 0,
-    //         min: -100,
-    //         max: 100,
-    //         step: 1,
-    //     },
-    // });
-
     return <Dog />;
 }
--- web/src/pages/dog-page/dog.tsx
+++ web/src/pages/dog-page/dog.tsx
@@ -1,21 +1,74 @@
-// import styled from '@emotion/styled';
-// import { Paper } from '@mui/material';
 // import Box from '@mui/material/Box';
 import { Canvas, useLoader } from '@react-three/fiber';
 import React, { ReactNode, Suspense, useEffect, useRef } from 'react';
 import { CameraControls, OrbitControls } from '@react-three/drei';
 import { BufferGeometry, Mesh, NormalBufferAttributes, Vector3 } from 'three';
 import { STLLoader } from 'three/examples/jsm/loaders/STLLoader';
-// import * as THREE from 'three';
-// const StyledPaper = styled(Paper)(({ theme }) => ({
-//     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fffFFF',
-// }));
+import * as THREE from 'three';
+import { useControls } from 'leva';
 
 // interface IProps {
-//     leg: number;
+//     shoulder4: number;
+//     shoulder3: number;
+//     shoulder2: number;
+//     shoulder1: number;
 // }
 
 export default function Dog() {
+    const control = useControls('shoulders', {
+        shoulder4: {
+            value: 0,
+            min: -90,
+            max: 90,
+            step: 1,
+        },
+        shoulder3: {
+            value: 0,
+            min: -90,
+            max: 90,
+            step: 1,
+        },
+        shoulder2: {
+            value: 0,
+            min: -90,
+            max: 90,
+            step: 1,
+        },
+        shoulder1: {
+            value: 0,
+            min: -90,
+            max: 90,
+            step: 1,
+        },
+    });
+
+    const control2 = useControls('knees', {
+        knee4: {
+            value: -90,
+            min: -150,
+            max: 0,
+            step: 1,
+        },
+        knee3: {
+            value: 90,
+            min: 0,
+            max: 150,
+            step: 1,
+        },
+        knee2: {
+            value: -90,
+            min: -150,
+            max: 0,
+            step: 1,
+        },
+        knee1: {
+            value: 90,
+            min: 0,
+            max: 150,
+            step: 1,
+        },
+    });
+
     const Model = ({
         url,
         color,
@@ -66,33 +119,97 @@ export default function Dog() {
                 {/* eslint-disable-next-line react/no-unknown-property */}
                 <pointLight visible castShadow position={[-1, 3, -1]} color="#FFFFFF" intensity={5} />
                 {/* eslint-disable-next-line react/no-unknown-property */}
-                <spotLight visible castShadow position={[-1, 5, -1]} color="#FFFFFF" intensity={5} />
+                <spotLight visible castShadow position={[-1, 8, 0]} color="#FFFFFF" intensity={5} />
             </>
         );
     }
 
     return (
-        <Canvas camera={{ position: [5, 5, -1] }}>
+        <Canvas camera={{ position: [7, 5, -1] }}>
             <Lights />
             <Suspense>
                 {/* eslint-disable-next-line react/no-unknown-property */}
-                <group position={[0, 1, 0]} scale={[0.01, 0.01, 0.01]}>
+                <group position={[0, 2.94, 0]} scale={[0.01, 0.01, 0.01]}>
                     <Model url={'models/dog_body/dog_front_simple.stl'}>
-                        <Model url={'models/dog_body/dog_middle_simple.stl'}>
-                            <Model url={'models/dog_body/dog_sidewall_simple.stl'}>
-                                <Model url={'models/dog_body/dog_back.stl'}>
-                                    <Model url={'models/dog_body/dog_sidewall_2.stl'}>
-                                        <Model url={'models/dog_body/dog_sidewall_3.stl'}>
-                                            <Model url={'models/dog_body/dog_sidewall_4.stl'}>
-                                                <Model url={'models/dog_body/dog_shoulder.stl'}>
-                                                    <Model url={'models/dog_body/dog_shoulder_2.stl'}>
-                                                        <Model url={'models/dog_body/dog_shoulder_3.stl'}>
-                                                            <Model url={'models/dog_body/dog_shoulder_4.stl'}></Model>
-                                                        </Model>
-                                                    </Model>
-                                                </Model>
-                                            </Model>
-                                        </Model>
+                        <Model
+                            url={'models/dog_body/dog_shoulder.stl'}
+                            point={new THREE.Vector3(0, 0, 0)}
+                            axis={new THREE.Vector3(0, 0, 1)}
+                            theta={(Math.PI * control.shoulder1) / 180}
+                        >
+                            <Model url={'models/dog_leg_1/1leg-dog_leg_simple.stl'}>
+                                <Model
+                                    url={'models/dog_leg_1/1leg-dog_knee_simple.stl'}
+                                    point={new THREE.Vector3(-195, 0, 0)}
+                                    axis={new THREE.Vector3(0, 0, 1)}
+                                    theta={(Math.PI * control2.knee1) / 180}
+                                >
+                                    <Model url={'models/dog_leg_1/1leg-dog_shin_simple.stl'}>
+                                        <Model url={'models/dog_leg_1/1leg-dog_foot_simple.stl'}></Model>
+                                    </Model>
+                                </Model>
+                            </Model>
+                        </Model>
+                        <Model
+                            url={'models/dog_body/dog_shoulder_2.stl'}
+                            point={new THREE.Vector3(186, 0, 0)}
+                            axis={new THREE.Vector3(0, 0, 1)}
+                            theta={(Math.PI * control.shoulder2) / 180}
+                        >
+                            <Model url={'models/dog_leg_2/2leg-dog_leg_simple.stl'}>
+                                <Model
+                                    url={'models/dog_leg_2/2leg-dog_knee_simple.stl'}
+                                    point={new THREE.Vector3(380, 0, 0)}
+                                    axis={new THREE.Vector3(0, 0, 1)}
+                                    theta={(Math.PI * control2.knee2) / 180}
+                                >
+                                    <Model url={'models/dog_leg_2/2leg-dog_shin_simple.stl'}>
+                                        <Model url={'models/dog_leg_2/2leg-dog_foot_simple.stl'}></Model>
+                                    </Model>
+                                </Model>
+                            </Model>
+                        </Model>
+                    </Model>
+                    <Model url={'models/dog_body/dog_middle_simple.stl'}></Model>
+                    <Model url={'models/dog_body/dog_sidewall_simple.stl'}></Model>
+                    <Model url={'models/dog_body/dog_sidewall_2.stl'}></Model>
+                    <Model url={'models/dog_body/dog_sidewall_3.stl'}></Model>
+                    <Model url={'models/dog_body/dog_sidewall_4.stl'}></Model>
+                    <Model url={'models/dog_body/dog_back.stl'}>
+                        <Model
+                            url={'models/dog_body/dog_shoulder_4.stl'}
+                            point={new THREE.Vector3(186, 0, 0)}
+                            axis={new THREE.Vector3(0, 0, 1)}
+                            theta={(Math.PI * control.shoulder4) / 180}
+                        >
+                            <Model url={'models/dog_leg_4/4leg-dog_leg_simple.stl'}>
+                                <Model
+                                    url={'models/dog_leg_4/4leg-dog_knee_simple.stl'}
+                                    point={new THREE.Vector3(380, 0, 0)}
+                                    axis={new THREE.Vector3(0, 0, 1)}
+                                    theta={(Math.PI * control2.knee4) / 180}
+                                >
+                                    <Model url={'models/dog_leg_4/4leg-dog_shin_simple.stl'}>
+                                        <Model url={'models/dog_leg_4/4leg-dog_foot_simple.stl'}></Model>
+                                    </Model>
+                                </Model>
+                            </Model>
+                        </Model>
+                        <Model
+                            url={'models/dog_body/dog_shoulder_3.stl'}
+                            point={new THREE.Vector3(0, 0, 0)}
+                            axis={new THREE.Vector3(0, 0, 1)}
+                            theta={(Math.PI * control.shoulder3) / 180}
+                        >
+                            <Model url={'models/dog_leg_3/3leg-dog_leg_simple.stl'}>
+                                <Model
+                                    url={'models/dog_leg_3/3leg-dog_knee_simple.stl'}
+                                    point={new THREE.Vector3(-196, 0, 0)}
+                                    axis={new THREE.Vector3(0, 0, 1)}
+                                    theta={(Math.PI * control2.knee3) / 180}
+                                >
+                                    <Model url={'models/dog_leg_3/3leg-dog_shin_simple.stl'}>
+                                        <Model url={'models/dog_leg_3/3leg-dog_foot_simple.stl'}></Model>
                                     </Model>
                                 </Model>
                             </Model>
@@ -117,7 +234,7 @@ export default function Dog() {
             {/* eslint-disable-next-line react/no-unknown-property */}
             <mesh position={[0, 0, 0]} rotation-x={-Math.PI / 2} receiveShadow castShadow>
                 {/* eslint-disable-next-line react/no-unknown-property */}
-                <planeGeometry args={[500, 500]} />
+                <planeGeometry args={[100, 100]} />
                 <meshStandardMaterial color="white" />
             </mesh>
             <OrbitControls />
