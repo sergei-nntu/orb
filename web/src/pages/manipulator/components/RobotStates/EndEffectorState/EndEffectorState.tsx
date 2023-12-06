50b8b9843fa86ef34646451d0cadb03e8044fa29
Maksim Kropanov <kropanov2012@gmail.com>
2023-12-06 12:30:39 +0300

    Fix align elements

--- web/src/pages/manipulator/components/RobotStates/EndEffectorState/EndEffectorState.tsx
+++ web/src/pages/manipulator/components/RobotStates/EndEffectorState/EndEffectorState.tsx
@@ -15,36 +15,46 @@ export default function EndEffectorState() {
     return (
         <StyledBox sx={{ mt: { md: 1, sm: 0 }, height: { md: '150px', xs: '280px' } }}>
             End-Effector State
-            <Grid container sx={{ mt: { md: 0, xs: 7 }, mb: 1 }}>
-                <Grid item xs={6}>
-                    <Typography noWrap variant="h6">
-                        <StyledTag>x</StyledTag>: {state?.position?.x || '0'}
-                    </Typography>
+            <Grid container justifyContent="center" sx={{ mt: { md: 0, xs: 7 }, mb: 1 }}>
+                {/* Первый столбец значений: x, y, z */}
+                <Grid item style={{ textAlign: 'right' }} sx={{ pl: 2, pr: 2 }}>
+                    <Grid container direction="column" alignItems="flex-start">
+                        <Grid item>
+                            <Typography noWrap variant="h6">
+                                <StyledTag>x</StyledTag>: {state?.position?.x || '0'}
+                            </Typography>
+                        </Grid>
+                        <Grid item>
+                            <Typography noWrap variant="h6">
+                                <StyledTag>y</StyledTag>: {state?.position?.y || '0'}
+                            </Typography>
+                        </Grid>
+                        <Grid item>
+                            <Typography noWrap variant="h6">
+                                <StyledTag>z</StyledTag>: {state?.position?.z || '0'}
+                            </Typography>
+                        </Grid>
+                    </Grid>
                 </Grid>
-                <Grid item xs={6}>
-                    <Typography noWrap variant="h6">
-                        <StyledTag>pitch</StyledTag>: {state?.orientation?.pitch || '0'}
-                    </Typography>
-                </Grid>
-                <Grid item xs={6}>
-                    <Typography noWrap variant="h6">
-                        <StyledTag>y</StyledTag>: {state?.position?.y || '0'}
-                    </Typography>
-                </Grid>
-                <Grid item xs={6}>
-                    <Typography noWrap variant="h6">
-                        <StyledTag>roll</StyledTag>: {state?.orientation?.roll || '0'}
-                    </Typography>
-                </Grid>
-                <Grid item xs={6}>
-                    <Typography noWrap variant="h6">
-                        <StyledTag>z</StyledTag>: {state?.position?.z || '0'}
-                    </Typography>
-                </Grid>
-                <Grid item xs={6}>
-                    <Typography noWrap variant="h6">
-                        <StyledTag>yaw</StyledTag>: {state?.orientation?.yaw || '0'}
-                    </Typography>
+                {/* Второй столбец значений: pitch, roll, yaw */}
+                <Grid item style={{ textAlign: 'left' }} sx={{ pl: 2, pr: 2 }}>
+                    <Grid container direction="column" alignItems="flex-start">
+                        <Grid item>
+                            <Typography noWrap variant="h6">
+                                <StyledTag>pitch</StyledTag>: {state?.orientation?.pitch || '0'}
+                            </Typography>
+                        </Grid>
+                        <Grid item>
+                            <Typography noWrap variant="h6">
+                                <StyledTag>roll</StyledTag>: {state?.orientation?.roll || '0'}
+                            </Typography>
+                        </Grid>
+                        <Grid item>
+                            <Typography noWrap variant="h6">
+                                <StyledTag>yaw</StyledTag>: {state?.orientation?.yaw || '0'}
+                            </Typography>
+                        </Grid>
+                    </Grid>
                 </Grid>
             </Grid>
         </StyledBox>
