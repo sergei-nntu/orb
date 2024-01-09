ff08b67999e02876dc27c10112f3e9cb2c26c0db
Maksim Kropanov <kropanov2012@gmail.com>
2024-01-09 15:22:40 +0300

    Get trajectory from the server

--- web/src/pages/navigation/Navigation.tsx
+++ web/src/pages/navigation/Navigation.tsx
@@ -1,5 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import useHttp from '../../hooks/Http/Http';
 
 export default function Navigation() {
+    // FIXME: only for testing
+    const { request } = useHttp();
+    useEffect(() => {
+        request('/get_joint_trajectory').then((r) => {
+            console.log(r);
+        });
+    }, []);
+
     return <h1>Navigation</h1>;
 }
