e122b38571ddef8ead4ea648689057d9d8c162fe
Maksim Kropanov <94756270+Kropanov@users.noreply.github.com>
2024-03-06 15:25:48 +0300

    Feature/usb enabled (#107)
    * Add usb notifications
    
    * Add usb status handler
    
    * Change usb connection interval
    
    * Add a new command to docker compose file
    
    * Fixes
    
    - Add disabled tabs
    - Fix tab naming
    - Rework calculating title
    - Add check on tabs (disabled) and no connection established
    
    * Fixes
    
    - Create feature to disable paths / routes
    - Add ProtectedRoute component
    - Append check to intervals (when there is no connection)
    
    * Fixes
    
    - Rewrite some logic in AppBar
    - Create disabled items for dev
    
    * Fixes
    
    - Delete protected route component
    - Rework disabled tabs and path logic
    - Add a new env variable
    - Append a new custom hook for usb connection
    
    * Fixes
    
    - Add ref currentPath
    - Create new a check on usb connection on manipulator and oqp pages

--- web/src/constants/index.ts
+++ web/src/constants/index.ts
@@ -21,12 +21,23 @@ export enum API_ROUTES {
     POST_JOINTS_STATE = '/post_joints_state',
     GET_BLOCKLY_STATE = '/get_blockly_state',
     GET_JOINT_TRAJECTORY = '/get_joint_trajectory',
+    GET_USB_CONNECTION_STATUS = '/get_usb_connection_status',
 }
 
 export enum KEY {
     BLOCKLY_CODE = 'BLOCKLY_CODE',
     BLOCKLY_STRUCTURE = 'BLOCKLY_STRUCTURE',
 }
+
+export enum TAB {
+    NAVIGATION = 'Navigation',
+    MANIPULATOR = 'Manipulator',
+    PLANNING = 'Planning',
+    QR = 'QR',
+    OQP = 'OQP',
+}
+
+export const DISABLED_TABS = [TAB.MANIPULATOR, TAB.OQP];
 
 export const INITIAL_POSE_STATE = {
     position: {
