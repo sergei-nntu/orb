3e114a001da46eb3addd647797efc5b94028b81e
Maksim Kropanov <kropanov2012@gmail.com>
2024-04-01 21:56:43 +0300

    Fixes
    - Fix package.json dependencies
    - Fix package.json
    - Fix a small drawback

--- web/src/pages/manipulator/components/RobotCamera/RobotCamera.tsx
+++ web/src/pages/manipulator/components/RobotCamera/RobotCamera.tsx
@@ -35,7 +35,7 @@ export default function RobotCamera() {
 
         return () => {
             if (videoElement) {
-                videoElement.removeEventListener('loadeddata', handleVideoLoaded);
+                videoElement.removeEventListener('load', handleVideoLoaded);
                 videoElement.removeEventListener('error', handleVideoError);
             }
         };
