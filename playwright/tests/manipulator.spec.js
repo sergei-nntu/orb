e9538c868a2400a54a5ae580d82a60c0e1970aa5
Svetlana Aristova <svetlana@2lemetry.ru>
2024-06-10 12:17:52 +0300

    Skip Joints Position tests

--- playwright/tests/manipulator.spec.js
+++ playwright/tests/manipulator.spec.js
@@ -97,7 +97,7 @@ test.describe('Orientation', () => {
     await bot._handledEditValueByClick( 'div:nth-child(2) > div > div:nth-child(3) > h6',["yaw"]);
   });
 });
-test.describe('Joints Position tests', () => {
+test.describe.skip('Joints Position tests', () => {
   test.describe.configure({ mode: 'serial' });
   test('Edit Joint0', async ({ page }) => {
     const bot = new Bot(page);
