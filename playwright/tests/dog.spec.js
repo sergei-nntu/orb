40c04c01957bbf676b48056ce2e1a3d5a0accda9
Svetlana Aristova <svetlana@2lemetry.ru>
2024-06-21 13:55:44 +0300

    Tests activated for OQP tab

--- playwright/tests/dog.spec.js
+++ playwright/tests/dog.spec.js
@@ -2,7 +2,7 @@ const { test, expect } = require('@playwright/test');
 const Bot = require("./bot");
 const shoulderValue = "-5";
 const reductorValue = "-45";
-const kneeValue = "95";
+const kneeValue = "85";
 
 test.beforeEach('Dog',async ({ page }) => {
     const bot = new Bot(page);
@@ -25,60 +25,60 @@ test.beforeEach('Dog',async ({ page }) => {
     }
 
 });
-test.describe.skip('Edit Front Left', () => {
+test.describe('Edit Front Left', () => {
 
     test('Edit Shoulder', async ({ page }) => {
         const bot = new Bot(page);
-        await bot._handledEditValueSlider('div:nth-child(1) > div:nth-child(3) > div > input', shoulderValue);
+        await bot._handledEditValueSlider('#input-joint-0', shoulderValue);
     });
     test('Edit Reductor', async ({ page }) => {
         const bot = new Bot(page);
-        await bot._handledEditValueSlider('div:nth-child(2) > div:nth-child(3) > div > input', reductorValue);
+        await bot._handledEditValueSlider('#input-joint-1', reductorValue);
     });
     test('Edit Knee', async ({ page }) => {
         const bot = new Bot(page);
-        await bot._handledEditValueSlider('div:nth-child(3) > div:nth-child(3) > div > input', kneeValue);
+        await bot._handledEditValueSlider('#input-joint-2', kneeValue);
     });
 });
-test.describe.skip('Edit Front Right', () => {
+test.describe('Edit Front Right', () => {
     test('Edit Shoulder', async ({ page }) => {
         const bot = new Bot(page);
-        await bot._handledEditValueSlider( 'div:nth-child(4) > div:nth-child(3) > div > input', shoulderValue);
+        await bot._handledEditValueSlider( '#input-joint-3', shoulderValue);
     });
     test('Edit Reductor', async ({ page }) => {
         const bot = new Bot(page);
-        await bot._handledEditValueSlider( ' div:nth-child(5) > div:nth-child(3) > div > input', reductorValue);
+        await bot._handledEditValueSlider( '#input-joint-4', reductorValue);
     });
     test('Edit Knee', async ({ page }) => {
         const bot = new Bot(page);
-        await bot._handledEditValueSlider('div:nth-child(6) > div:nth-child(3) > div > input', kneeValue);
+        await bot._handledEditValueSlider('#input-joint-5', kneeValue);
     });
 });
-test.describe.skip('Edit Rear Left', () => {
+test.describe('Edit Rear Left', () => {
     test('Edit Shoulder', async ({ page }) => {
         const bot = new Bot(page);
-        await bot._handledEditValueSlider( 'div:nth-child(7) > div:nth-child(3) > div > input', shoulderValue);
+        await bot._handledEditValueSlider( '#input-joint-6', shoulderValue);
     });
     test('Edit Reductor', async ({ page }) => {
         const bot = new Bot(page);
-        await bot._handledEditValueSlider('div:nth-child(8) > div:nth-child(3) > div > input', reductorValue);
+        await bot._handledEditValueSlider('#input-joint-7', reductorValue);
     });
     test('Edit Knee', async ({ page }) => {
         const bot = new Bot(page);
-        await bot._handledEditValueSlider('div:nth-child(9) > div:nth-child(3) > div > input', kneeValue);
+        await bot._handledEditValueSlider('#input-joint-8', kneeValue);
     });
 });
-test.describe.skip('Edit Rear Right', () => {
+test.describe('Edit Rear Right', () => {
     test('Edit Shoulder', async ({ page }) => {
         const bot = new Bot(page);
-        await bot._handledEditValueSlider( 'div:nth-child(10) > div:nth-child(3) > div > input', shoulderValue);
+        await bot._handledEditValueSlider( '#input-joint-9', shoulderValue);
     });
     test('Edit Reductor', async ({ page }) => {
         const bot = new Bot(page);
-        await bot._handledEditValueSlider( 'div:nth-child(11) > div:nth-child(3) > div > input', reductorValue);
+        await bot._handledEditValueSlider( '#input-joint-10', reductorValue);
     });
     test('Edit Knee', async ({ page }) => {
         const bot = new Bot(page);
-        await bot._handledEditValueSlider( 'div:nth-child(12) > div:nth-child(3) > div > input', kneeValue);
+        await bot._handledEditValueSlider( '#input-joint-11', kneeValue);
     });
 });
