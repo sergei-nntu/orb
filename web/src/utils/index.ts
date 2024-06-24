dae2893ad7e3f850bd724fd8d8a607a2fe650ecd
Dmitriy8998 <dmitry.chistov@2lemetry.ru>
2024-06-24 16:13:50 +0300

    refactor: remove unused comments

--- web/src/utils/index.ts
+++ web/src/utils/index.ts
@@ -5,13 +5,3 @@ export function convertDeegreToRadian(value: number) {
 export function convertRadianToDegree(value: number) {
     return +((value * 180) / Math.PI).toFixed(0);
 }
-
-// export const updateFlagsLoading = (axis: string, value: boolean, type: 'position' | 'orientation') => {
-//     setFlagsLoading((prev) => ({
-//         ...prev,
-//         [type]: {
-//             ...prev[type],
-//             [type === 'position' ? `flagLoading${axis.toUpperCase()}` : `flagLoading${axis}`]: value,
-//         },
-//     }));
-// };
