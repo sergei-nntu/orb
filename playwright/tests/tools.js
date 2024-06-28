05507b6bcbff442c3b191c077de7d00636a3d279
Svetlana Aristova <svetlana@2lemetry.ru>
2024-06-28 13:44:00 +0300

    Edited playwright.yml

--- playwright/tests/tools.js
+++ playwright/tests/tools.js
@@ -80,8 +80,8 @@ module.exports = class Tools {
         await this.element.reload();
         await this._waitLoading();
 
-        // await this.element.waitForTimeout(2000);
+        await this.element.waitForTimeout(3000);
 
-        await this.element.locator('//*[(contains(@class,\'disabled\'))] //*[(contains(@id,\'input-joint\'))]').last().waitFor({state:'hidden'});
+        // await this.element.locator('//*[(contains(@class,\'disabled\'))] //*[(contains(@id,\'input-joint\'))]').last().waitFor({state:'hidden'});
     }
 };
